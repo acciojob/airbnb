c14325c3694b45172a5f1794f904ccf73d5d60ce
Vishwas Goyal <vishwas.goyal@phonepe.com>
2023-04-23 22:28:42 +0530

    tc added

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,15 +1,12 @@
 package com.driver.test;
 
 import com.driver.EaseMyTrip;
-import com.driver.controllers.AirportController;
+import com.driver.controllers.HotelManagementController;
 import com.driver.model.*;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.*;
@@ -17,213 +14,92 @@ import java.util.*;
 @SpringBootTest(classes = EaseMyTrip.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCases {
-    AirportController airportController = new AirportController();
+    HotelManagementController hotelManagementController = new HotelManagementController();
+
     @Test
-    public void testAddAirport(){
-        String result = airportController.addAirport(new Airport("IGI2",2,City.DELHI));
+    public void testAddHotel(){
+        String result = hotelManagementController.addHotel(getHotel("Tushar",new ArrayList<>()));
         Assertions.assertEquals("SUCCESS",result);
     }
-
     @Test
-    public void testGetLargestAirportName(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        String airportName = airportController.getLargestAirportName();
-        Assertions.assertEquals("KIA",airportName);
+    public void testAddHotelWhenNameNull(){
+        String result = hotelManagementController.addHotel(getHotel(null,new ArrayList<>()));
+        Assertions.assertEquals("FAILURE",result);
     }
 
     @Test
-    public void testGetLargestAirportNameInCaseOfSameNumberOfTerminals(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addAirport(new Airport("BIA",4,City.BANGLORE));
-        String airportName = airportController.getLargestAirportName();
-        Assertions.assertEquals("BIA",airportName);
-    }
-    @Test
-    public void testGetShortestDurationOfPossibleBetweenTwoCities(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-
-        double duration = airportController.getShortestDurationOfPossibleBetweenTwoCities(City.CHANDIGARH,City.BANGLORE);
-        Assertions.assertEquals(2.5,duration);
-
-    }
-    @Test
-    public void testGetShortestDurationOfPossibleBetweenTwoCitiesWhenFightDontExist(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        double duration = airportController.getShortestDurationOfPossibleBetweenTwoCities(City.CHENNAI,City.BANGLORE);
-        Assertions.assertEquals(-1,duration);
-
-    }
-    @Test
-    public void testGetNumberOfPeopleOn(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        int numberOfPeopleOn = airportController.getNumberOfPeopleOn(new Date(2023,3,27),"KIA");
-        Assertions.assertEquals(2,numberOfPeopleOn);
-    }
-    @Test
-    public void testGetNumberOfPeopleOnWithNoFlight(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        int numberOfPeopleOn = airportController.getNumberOfPeopleOn(new Date(2023,3,27),"A");
-        Assertions.assertEquals(0,numberOfPeopleOn);
-    }
-    @Test
-    public void testCalculateFlightFare(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        int flightFare = airportController.calculateFlightFare(1);
-        Assertions.assertEquals(3050,flightFare);
-    }
-    @Test
-    public void testBookAFlight(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        String result = airportController.bookATicket(1,2);
-        Assertions.assertEquals("SUCCESS", result);
-    }
-    @Test
-    public void testBookAFlightWhenPassengerAlreadyThere(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        String result = airportController.bookATicket(1,1);
-        Assertions.assertEquals("FAILURE", result);
+    public void testAddHotelWhenHotelAlreadyExists(){
+        hotelManagementController.addHotel(getHotel("Tushar",new ArrayList<>()));
+        String result = hotelManagementController.addHotel(getHotel("Tushar",new ArrayList<>()));
+        Assertions.assertEquals("FAILURE",result);
     }
 
     @Test
-    public void testCancelATicket(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        String result = airportController.cancelATicket(1,1);
-        Assertions.assertEquals("SUCCESS", result);
+    public void testAddUser(){
+        int aadharNumber = hotelManagementController.addUser(getUser(1,"Tushar", 22));
+        Assertions.assertEquals(1,aadharNumber);
+    }
 
+    @Test
+    public void testGetHotelWithMaxFacilities(){
+        List<Facility> facilities = new ArrayList<>();
+        facilities.add(Facility.BAR);
+        hotelManagementController.addHotel(getHotel("Tushar",facilities));
+        List<Facility> facilities1 = new ArrayList<>();
+        facilities1.add(Facility.GYM);
+        facilities1.add(Facility.BAR);
+        hotelManagementController.addHotel(getHotel("Tushar1",facilities1));
+        String hotelName = hotelManagementController.getHotelWithMostFacilities();
+        Assertions.assertEquals("Tushar1",hotelName);
     }
     @Test
-    public void testCancelATicketWhenTicketDoesntExist(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        String result = airportController.cancelATicket(2,1);
-        Assertions.assertEquals("FAILURE", result);
+    public void testGetHotelWithMaxFacilitiesWhereNoFacilities(){
+        hotelManagementController.addHotel(getHotel("Tushar",new ArrayList<>(){}));
+        String hotelName = hotelManagementController.getHotelWithMostFacilities();
+        Assertions.assertEquals("",hotelName);
     }
-    @Test
-    public void testAddFlight(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        String result = airportController.addFlight(new Flight(3,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        Assertions.assertEquals("SUCCESS", result);
 
-    }
     @Test
-    public void testGetAirportNameFromFlightId(){airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
+    public void testGetARoom(){
+        hotelManagementController.addHotel(getHotel("T",new ArrayList<>()));
+        int result = hotelManagementController.bookARoom(getBooking(1,2,"Tushar","T"));
+        Assertions.assertEquals(2000,result);
+    }
 
-        String airportName = airportController.getAirportNameFromFlightId(1);
-        Assertions.assertEquals("CA", airportName);
-    }
     @Test
-    public void testGetAirportNameFromFlightIdWhenAirportDoesntExist(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        String airportName = airportController.getAirportNameFromFlightId(3);
-        Assertions.assertEquals(null, airportName);
+    public void testGetARoomWhenRoomsNotAvailable(){
+        hotelManagementController.addHotel(getHotel("T",new ArrayList<>()));
+        int result = hotelManagementController.bookARoom(getBooking(1,6,"Tushar","T"));
+        Assertions.assertEquals(-1, result);
     }
-    @Test
-    public void testCalculateRevenueOfAFlight(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        int revenue = airportController.calculateRevenueOfAFlight(1);
-        Assertions.assertEquals(3000, revenue);
-    }
-    @Test
-    public void testAddPassenger(){
-        String result = airportController.addPassenger(new Passenger(3,"e3","p3",15));
-        Assertions.assertEquals("SUCCESS", result);
 
+    @Test
+    public void testGetBookings(){
+        hotelManagementController.addHotel(getHotel("T",new ArrayList<>()));
+        hotelManagementController.bookARoom(getBooking(1,1,"Tushar","T"));
+        hotelManagementController.bookARoom(getBooking(1,1,"Tushar","T"));
+        int result = hotelManagementController.getBookings(1);
+        Assertions.assertEquals(2, result);
     }
     @Test
-    public void testCountOfBookingsDoneByPassengerAllCombined(){
-        airportController.addAirport(new Airport("IGI",2,City.DELHI));
-        airportController.addAirport(new Airport("CA",3,City.CHANDIGARH));
-        airportController.addAirport(new Airport("KIA",4,City.BANGLORE));
-        airportController.addPassenger(new Passenger(1,"e1","p1",12));
-        airportController.addPassenger(new Passenger(2,"e2","p2",15));
-        airportController.addFlight(new Flight(1,City.CHANDIGARH,City.BANGLORE,100,new Date(2023,3,27),2.5));
-        airportController.addFlight(new Flight(2,City.DELHI,City.BANGLORE,100,new Date(2023,3,27),2.0));
-        airportController.bookATicket(1,1);
-        airportController.bookATicket(2,2);
-        airportController.bookATicket(2,1);
-        int count = airportController.countOfBookingsDoneByPassengerAllCombined(1);
-        Assertions.assertEquals(2, count);
+    public void testUpdateFacilities(){
+        hotelManagementController.addHotel(getHotel("T",new ArrayList<>()));
+        List<Facility> facilities = new ArrayList<>();
+        facilities.add(Facility.GYM);
+        facilities.add(Facility.BAR);
+        Hotel hotel = hotelManagementController.updateFacilities(facilities,"T");
+        Assertions.assertEquals(2, hotel.getFacilities().size());
+    }
+
+    public Hotel getHotel(String name,List<Facility> facilities){
+        return new Hotel(name,5,facilities,1000);
+    }
+    public User getUser(int aadharNumber, String name, int age) {
+        return new User(aadharNumber, name, age);
+    }
+
+    public Booking getBooking(int bookingAadharCard,int noOfRooms, String name ,String hotelName){
+        return new Booking(bookingAadharCard,noOfRooms,name,hotelName);
     }
 }
 
